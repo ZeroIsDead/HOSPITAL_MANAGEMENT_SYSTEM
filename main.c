cf2c04de99be733ae2fb4ebea73e69ee83557eae
GoatKy1e <meekail.mugdho@gmail.com>
2024-05-14 12:37:44 +0800

    Main menue bug fix 01

--- main.c
+++ main.c
@@ -4,42 +4,41 @@ int main()
 {
     char* header = "WELCOME TO THE HOSPITAL";
     char* options[] = {"Patient Login", "Staff Login"};
-    char* output; 
+    int output; 
 
     clearTerminal();
     output = displayMenu(header, options, 2);
 
-    if (!strncmp(output, options[0], 255))
+    if (output == 1)
     {
         char* header2 = "HOW CAN I HELP TODAY";
         char* options2[] = {"Option1", "Option2", "Option3", "Option4"};
-        char* output2;
+        int output2;
 
         clearTerminal();
         output2 = displayMenu(header, options2, 4);
-        printf(output2);2
+        printf("%d", output2);
 
     }
-    else if (!strncmp(output, options[1], 255))
+    else if (output == 2)
     {
         char* header3 = "SELEKT YOUR ROLE";
         char* options4[] = {"Doctor", "Nurse", "Admin"};
-        char* output3;
+        int output3;
         
         clearTerminal();
         output3 = displayMenu(header3, options4, 3);
-        printf(output3);
-        if (!strncmp(output, options4[0], 255))
+        if (output3 == 1)
         {
             printf("Doctor");
             //doctor() starts here
         }
-        else if (!strncmp(output, options4[1], 255))
+        else if (output3 == 2)
         {
             printf("Nurse");
             //nurse() starts here
         }
-        else if (!strncmp(output, options4[2], 255))
+        else if (output3 == 3)
         {
             printf("Admin");
             //admin() starts here
