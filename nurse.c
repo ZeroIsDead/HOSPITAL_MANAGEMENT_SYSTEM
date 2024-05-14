53eb9b05a60f966742342c49dea84857eaf8cb5c
GoatKy1e <meekail.mugdho@gmail.com>
2024-05-14 10:59:13 +0800

    Nurse Menu

--- nurse.c
+++ nurse.c
@@ -1,1 +1,25 @@
 #include "File_exe.h"
+
+char* message(char name []){ 
+    
+    int welcomeSize = 8 + strlen(name) + 1;
+    char welcome[welcomeSize];
+    
+    strcpy(welcome, "Welcome ");
+    strcat(welcome, name);
+    
+    welcome[welcomeSize-1] = '\0';
+
+    return strdup(welcome);
+    
+}
+
+int main(){
+    char name []= "mugdho";
+    char* header = message(name);
+    char* options[] = {"PEPE","POPO","PEPEPOPO"};
+
+    int output = displayMenu(header, options, 3);
+
+    clearTerminal();
+}
