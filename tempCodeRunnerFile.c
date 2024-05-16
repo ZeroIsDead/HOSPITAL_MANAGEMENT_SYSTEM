31f94dd8785dca35924f0a67f04fc0c8149cab31
Not Irfan <irfan.nrrw7@gmail.com>
2024-05-16 10:00:33 +0800

    added admin menu

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,39 @@
+#include "File_exe.h"
+
+int main() 
+{
+    char* header = "Welcome to the Matrix";
+    char* option[] = {"Registration", "Appointment", "Inventory"};
+    char* output;
+    
+    clearTerminal();
+    output = displayMenu(header, option, 3);
+
+    if (output==1)
+    {
+        char* header2 = "Registration Menu";
+        char* options2[] = {"Patient","Doctor","Nurse"};
 
         clearTerminal();
+        int output2 = displayMenu(header, options2, 3);
+    }
+
+    else if (!strncmp(output, option[1], 255))
+    {
+        char* header3 = "Appointment Menu";
+        char* options4[] = {"View Active Appointment List", "View Past Appointment List"};
+        
+        clearTerminal();
+        int output3 = displayMenu(header3, options4, 2);
+        printf(output3);
+    }
+
+    else if (!strncmp(output, option[2], 255))
+    {
+        char* header3 = "Inventory Menu";
+        char* options4[] = {"View Inventory", "Search Inventory", "Restock Inventory"};
+        
+        clearTerminal();
+        int output3 = displayMenu(header3, options4, 3);
+    }
+}
