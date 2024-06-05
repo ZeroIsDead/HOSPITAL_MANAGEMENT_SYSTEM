d4965ef21ad05a05eb236ecc63e9a294ee38bff7
GoatKy1e <meekail.mugdho@gmail.com>
2024-06-05 17:42:55 +0800

    Main: - fixed Main - Created StaffPortal

--- main.c
+++ main.c
@@ -9,6 +9,31 @@
 ////////////////////admin.c/////////////////////////
 
 /////////////////////main.c////////////////////////
+void StaffPortal(){
+    char* header = "Welcome Staff";
+    char* options[] = {"Admin", "Doctor", "Nurse", "Back"};
+    int output = displayMenu(header,options,4);
+    clearTerminal();
+
+    if (output == 1){
+        //Call AdminMain
+        printf("ADMIN PORTAL\n");
+    }
+    else if (output == 2){
+        // Call DoctorMain
+        printf("DOCTOR PORTAL\n");
+    }
+    else if (output == 3){
+        // Call NurseMain
+        printf("NURSE PORTAL\n");
+    }
+    else {
+        clearTerminal();
+        main();
+        return;
+    }
+}
+
 int main() 
 {
     char* header = "WELCOME TO THE HOSPITAL";
@@ -20,39 +45,17 @@ int main()
 
     if (output == 1)
     {
-        char* header2 = "HOW CAN I HELP TODAY";
-        char* options2[] = {"Option1", "Option2", "Option3", "Option4"};
-        int output2;
-
-        clearTerminal();
-        output2 = displayMenu(header, options2, 4);
-        printf("%d", output2);
+        
+        int paitent = 2;// Call Paitent Main
+        while (paitent == 1){
+            displaySystemMessage("WRONG! PAITENT!",5);
+            paitent = 2;//Call Paitent Main
+        }
 
     }
     else if (output == 2)
     {
-        char* header3 = "SELEKT YOUR ROLE";
-        char* options4[] = {"Doctor", "Nurse", "Admin"};
-        int output3;
-        
-        clearTerminal();
-        output3 = displayMenu(header3, options4, 3);
-        if (output3 == 1)
-        {
-            printf("Doctor");
-            //doctor() starts here
-        }
-        else if (output3 == 2)
-        {
-            printf("Nurse");
-            //nurse() starts here
-        }
-        else if (output3 == 3)
-        {
-            printf("Admin");
-            //admin() starts here
-        }
-        
-        
+        StaffPortal();
+        return 0;
     }
 }
